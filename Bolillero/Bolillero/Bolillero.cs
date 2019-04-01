d5a5cf23643f0c9f9900c4777fbf565ed69e71c8
orlndo33 <joseapaza33@gmail.com>
2019-04-01 17:08:47 -0300

    Agregado de codigo
    codigo

---
+++ Bolillero/Bolillero/Bolillero.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bolillero
+{
+    public class Bolillero
+    {
+        Random r;
+        List<int> bolillasD { get; set; }
+        List<int> bolillasA { get; set; }
+
+        public Bolillero()
+        {
+            bolillasA = new List<int>();
+            bolillasD = new List<int>();
+        }
+
+        public int sacarBolilla()
+        {
+            int s = r.Next(0, bolillasD.Count);
+            bolillasD.RemoveAt(s);
+            
+            return s;
+        }
+
+        public int 
+    }
+}
